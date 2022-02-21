1e8055197619d7797e538ba01899cba2052ba300
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-02-21 19:56:34 +0530

    Added: code The Employee is Presentg or Absent

--- src/day8/EmpWageComputation.java
+++ src/day8/EmpWageComputation.java
@@ -1,9 +1,18 @@
 package day8;
 
+import java.util.Random; // import Random class
+
 public class EmpWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-	}
+		Random random = new Random();
+		int r = random.nextInt(2);
+		if (r == 1)
+			//Employee Present or Absent
+			System.out.println("Employee Present");
+		else
+			System.out.println("Employee Absent");
 
+	}
 }
